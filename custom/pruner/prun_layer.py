399c77536cbb0ac36e80953cc6bf417edd572f37
Kiên <kien.tran8@onemount.com>
2025-12-30 23:03:32 +0700

    update

--- custom/pruner/prun_layer.py
+++ custom/pruner/prun_layer.py
@@ -277,7 +277,7 @@ def main():
 
     # Model configuration
     parser.add_argument(
-        '--model', type=str, default='meta-llama/Llama-3.2-1B',
+        '--model', type=str, default='MiniCPM-2B-sft-bf16',
         help='HuggingFace model identifier'
     )
     parser.add_argument(
